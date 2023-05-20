e191ee79efa4954067ae368f9522cd1eefe9233c
AndreyNesterenko2020 <74727724+AndreyNesterenko2020@users.noreply.github.com>
2023-05-20 14:31:23 -0400

    Add files via upload

--- proxy.php
+++ proxy.php
@@ -1,7 +1,10 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 $file=fopen("JOBS_IN", "a");
-$job_count = count(explode(",", fread(fopen("JOBS_IN", "r"),filesize("JOBS_IN"))));
+$job_count = 0;
+if(filesize("JOBS_IN") > 0) {
+  $job_count = count(explode(",", fread(fopen("JOBS_IN", "r"),filesize("JOBS_IN"))));
+}
 $random_hash = bin2hex(random_bytes(18));
 $data_in = ($job_count-1>0 ? "\n" : "").$random_hash.":".base64_encode(explode("?", $_SERVER['REQUEST_URI'])[1]).",";
 fwrite($file, $data_in);
@@ -10,7 +13,10 @@ for (;;) {
    //job finished?
    clearstatcache();
    $response=fopen("JOBS_OUT", "r");
-   $contents=fread($response,filesize("JOBS_OUT"));
+   $contents="";
+   if(filesize("JOBS_OUT") > 0) {
+     $contents = fread($response,filesize("JOBS_OUT"));
+   }
    if(strpos($contents, $random_hash) !== false) {
      $data = trim(explode(",", explode($random_hash, $contents)[1])[0], ":");
      $fileWriteIn = fopen("JOBS_IN", "w");
